e9089a48627216afdaa17d08a2c3761ac73d6ed0
Dave St.Germain <dstgermain@edx.org>
2019-11-15 10:41:36 -0500

    Added new role for course data researcher.

--- common/djangoapps/student/roles.py
+++ common/djangoapps/student/roles.py
@@ -313,6 +313,14 @@ class CourseCcxCoachRole(CourseRole):
         super(CourseCcxCoachRole, self).__init__(self.ROLE, *args, **kwargs)
 
 
+class CourseDataResearcherRole(CourseRole):
+    """A Data Researcher"""
+    ROLE = 'data_researcher'
+
+    def __init__(self, *args, **kwargs):
+        super(CourseDataResearcherRole, self).__init__(self.ROLE, *args, **kwargs)
+
+
 class OrgStaffRole(OrgRole):
     """An organization staff member"""
     def __init__(self, *args, **kwargs):
