e0a3209ecc2ccfaa3ba23acffcadefbaa7bf81bf
sysadmintma <sysadmin@themoocagency.com>
2022-03-23 09:48:16 +0000

    update certificate views to add color

--- lms/djangoapps/wul_apps/certificates/views.py
+++ lms/djangoapps/wul_apps/certificates/views.py
@@ -53,49 +53,22 @@ def render_partial(request,course_id):
 def generate_pdf(request,course_id):
 
     #import value for the certificate
-    try:
-        user_name = (request.user.first_name).capitalize() + " " + (request.user.last_name).capitalize()
-    except:
-        try:
-            user_name = json.loads(request.user.profile.custom_field).get('first_name') + " " + json.loads(request.user.profile.custom_field).get('last_name')
-        except:
-            user_name = 'Missing information'
-
-
     certificate_config = configuration_helpers.get_value('CERTIFICATE_LAYOUT')[course_id]
 
+
+
+    # Setup SIZE, IMAGE, FONT and COLOR
     page_width = certificate_config['certificate_width']
     page_height = certificate_config['certificate_height']
-    name_position_x = certificate_config['name_position_x']
-    font_size = certificate_config['font_size']
     image_url = certificate_config['certificate_url']
-
-    try:
-        name_position_y = certificate_config['name_position_y']
-    except:
-        name_position_y = False
-
     try:
         font_name = certificate_config['font_name']
         font_url = certificate_config['font_url']
     except:
         font_name = 'OpenSans'
         font_url = "/edx/var/edxapp/staticfiles/fonts/OpenSans/OpenSans-Regular-webfont.ttf"
-
-    try:
-        certificate_date = certificate_config['date']
-    except:
-        certificate_date = False
-
-    try:
-        date_lang = certificate_config['date_lang'].lower()
-    except:
-        date_lang = 'fr'
-
-
     #import custom font
     pdfmetrics.registerFont(TTFont(font_name,font_url))
-
     # Create the HttpResponse object with the appropriate PDF headers.
     response = HttpResponse(content_type='application/pdf')
     response['Content-Disposition'] = 'attachment; filename="certificat.pdf"'
@@ -107,8 +80,29 @@ def generate_pdf(request,course_id):
     # Draw the image at x, y. I positioned the x,y to be where i like here
     p.drawImage(image_url, 0, 0, width=page_width,height=page_height)
     # Draw things on the PDF. Here's where the PDF generation happens.
-    p.setFont(font_name, font_size)
 
+
+
+    # USER NAME
+    name_position_x = certificate_config['name_position_x']
+    font_size = certificate_config['font_size']
+    p.setFont(font_name, font_size)
+    try:
+        font_color = certificate_config['font_color']
+    except:
+        font_color = [255, 255, 255]
+    p.setFillColorRGB(font_color[0]/255, font_color[1]/255, font_color[2]/255) 
+    try:
+        user_name = (request.user.first_name).capitalize() + " " + (request.user.last_name).upper()
+    except:
+        try:
+            user_name = json.loads(request.user.profile.custom_field).get('first_name').capitalize() + " " + json.loads(request.user.profile.custom_field).get('last_name').upper()
+        except:
+            user_name = 'Missing information'
+    try:
+        name_position_y = certificate_config['name_position_y']
+    except:
+        name_position_y = False
     if name_position_y :
         p.drawString(name_position_y, name_position_x, user_name)
     else:
@@ -118,31 +112,40 @@ def generate_pdf(request,course_id):
         p.drawString(centered_text, name_position_x, user_name)
 
 
-    if certificate_date :
 
+    # CERTIFICATE DATE
+    try:
+        certificate_date = certificate_config['date']
+    except:
+        certificate_date = False
+    try:
+        date_lang = certificate_config['date_lang'].lower()
+    except:
+        date_lang = 'fr'
+
+    if certificate_date :
         today = date.today()
         string_date_en = today.strftime('%d %B %Y')
 
         english_months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
         french_months = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre']
-
         for index, e in enumerate(english_months):
             if string_date_en.find(e) != -1 :
                 string_date_fr = string_date_en.replace(e, french_months[index])
 
-        # Add switch to multilingue ? 
+        # Switch to multilingue
         if date_lang == 'en' :
             certificate_date = string_date_en
         else:
             certificate_date = 'le ' + string_date_fr
-
+        # Write date at x and y with font_size_2
         date_position_x = certificate_config['date_position_x']
         try:
             date_position_y = certificate_config['date_position_y']
         except:
             date_position_y = False
-
         font_size_2 = certificate_config['font_size_2']
+
         p.setFont(font_name, font_size_2)
         if date_position_y:
             p.drawString(date_position_y, date_position_x , certificate_date)
@@ -150,6 +153,8 @@ def generate_pdf(request,course_id):
             text_width_date = stringWidth(certificate_date, font_name, font_size_2)
             centered_date = (page_width - text_width_date) / 2.0
             p.drawString(centered_date, date_position_x , certificate_date)
+
+
 
     # Close the PDF object cleanly, and we're done.
     p.showPage()
