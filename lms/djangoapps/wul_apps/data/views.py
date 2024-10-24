cc1d3b60c9768bbbac6814ea7b1116e7b30f6712
dimitri-hoareau-WEL <dimitri.hoareau@weuplearning.com>
2024-10-24 12:52:13 +0000

    19542 - email template and csv data api to update

--- lms/djangoapps/wul_apps/data/views.py
+++ lms/djangoapps/wul_apps/data/views.py
@@ -1,15 +1,56 @@
 import os
 
+from django.views.decorators.http import require_http_methods
+from django.views.decorators.csrf import csrf_exempt
+
 from django.http import HttpResponse
 from datetime import datetime
+import json
+import hashlib
+import hmac
 
 import logging
 log = logging.getLogger()
 
 
+SECRET_KEY = 'wAl%Nx<6Rq/$f6-!7jQFB4mO)P'
+
+def data_check(signature, data):
+    """
+    Validates the provided signature using HMAC and a shared secret key.
+    """
+
+    # Generate HMAC signature based on the data and the SECRET_KEY
+    expected_signature = hmac.new(
+        SECRET_KEY.encode(),
+        data.encode(),
+        hashlib.sha256
+    ).hexdigest()
+
+    # log.info("expected_signature")
+    # log.info(expected_signature)
+    # log.info("signature")
+    # log.info(signature)
+
+    return hmac.compare_digest(expected_signature, signature)
+
+
+
+@csrf_exempt
+@require_http_methods(["POST"])
 def csvDataWeup(request, course_id):
-    # will respond :
-    # https://af-brazil.weup.in/wul_apps/csv_data_weup/course-v1:af-brasil+july+2024
+    """
+    Will respond :
+        https://af-brazil.weup.in/wul_apps/csv_data_weup/course-v1:af-brasil+PP+TB
+    """
+
+    body_unicode = request.body.decode('utf-8')
+    body_data = json.loads(body_unicode)
+    signature = body_data.get('signature')
+
+    if not signature or not data_check(signature, course_id):
+        return HttpResponse('Unauthorized', status=403)
+
 
     csv_dir_path = '/edx/var/edxapp/media/microsites/af-brazil/csv/'+ course_id
     today_date = datetime.now().strftime('%Y_%m_%d')
@@ -22,6 +63,5 @@ def csvDataWeup(request, course_id):
             return response
     else:
         return HttpResponse('CSV file not found', status=404)
-
 
 
